8dc56ab029159882540505162d2a05058999d5ce
sami561 <samiayachi653@gmail.com>
2023-01-29 16:05:35 +0100

    daily page

--- front/src/App.js
+++ front/src/App.js
@@ -10,6 +10,8 @@ import  Product from  'scenes/products'
 import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/Geography";
+import Overview from "scenes/overview";
+import Daily from "scenes/Daily";
 
 function App() {
   const  mode=useSelector((state)=>state.global.mode);
@@ -28,6 +30,8 @@ function App() {
               <Route path="/customers" element={<Customers />} />
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
+              <Route path="/Overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
           </Route>
         </Routes>
       </ThemeProvider>
